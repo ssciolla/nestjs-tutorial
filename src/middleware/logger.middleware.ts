b0f2e90271d5ec8efc1c2f3cbacc2d9b1885c785
Sam Sciolla <ssciolla@umich.edu>
2021-04-23 16:02:01 -0400

    Fix string format for request logging middleware

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log(
-      `Route: ${req.originalUrl} - Method: ${req.method} - IP Address: ${req.ip} - `,
+      `Route: ${req.originalUrl} - Method: ${req.method} - IP Address: ${req.ip}`,
     );
     next();
   }
