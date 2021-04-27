2850557fa4bd8da7118da18d3192a72dc171638e
Sam Sciolla <ssciolla@umich.edu>
2021-04-27 10:00:31 -0400

    Add ApiProperty decarators to DTOs

--- src/cats/cats.dto.ts
+++ src/cats/cats.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCatDto {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   age: number;
+
+  @ApiProperty()
   breed: string;
 
   constructor(name: string, age: number, breed: string) {
